4c73a34937bc387abb64a3bd5a323894a55bdd67
iii80 <myfriends123@gmail.com>
2024-05-17 15:59:14 +0800

    Update V2bX.sh

--- V2bX.sh
+++ V2bX.sh
@@ -55,7 +55,7 @@ update() {
     else
         version=$2
     fi
-    bash <(curl -Ls https://raw.githubusercontent.com/iii80/V2bX-script/master/install.sh) $version
+    bash <(curl -Ls https://raw.githubusercontent.com/iii80/V2bX-script-alpine/master/install.sh) $version
     if [[ $? == 0 ]]; then
         echo -e "${green}更新完成，已自动重启 V2bX，请使用 V2bX log 查看运行日志${plain}"
         exit
