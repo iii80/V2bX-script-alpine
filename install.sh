1ff361941134de9d9903c3a26ac0af897e3fc3a5
iii80 <myfriends123@gmail.com>
2024-05-17 16:03:20 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -141,7 +141,7 @@ install_V2bX() {
     if [[ ! -f /etc/V2bX/custom_inbound.json ]]; then
         cp custom_inbound.json /etc/V2bX/
     fi
-    curl -o /usr/bin/V2bX -Ls https://raw.githubusercontent.com/iii80/V2bX-script/master/V2bX.sh
+    curl -o /usr/bin/V2bX -Ls https://raw.githubusercontent.com/iii80/V2bX-script-alpine/master/V2bX.sh
     chmod +x /usr/bin/V2bX
     if [ ! -L /usr/bin/v2bx ]; then
         ln -s /usr/bin/V2bX /usr/bin/v2bx
@@ -177,7 +177,7 @@ install_V2bX() {
     if [[ $first_install == true ]]; then
         read -rp "检测到你为第一次安装V2bX,是否自动直接生成配置文件？(y/n): " if_generate
         if [[ $if_generate == [Yy] ]]; then
-            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/iii80/V2bX-script/master/initconfig.sh
+            curl -o ./initconfig.sh -Ls https://raw.githubusercontent.com/iii80/V2bX-script-alpine/master/initconfig.sh
             source initconfig.sh
             rm initconfig.sh -f
             generate_config_file
